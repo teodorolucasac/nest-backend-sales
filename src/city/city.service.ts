0a99f65331b5c0bf4a1751ca2f224a621cd503cc
Lucas Teodoro <lucasteodoroac@gmail.com>
2023-04-06 12:47:00 -0300

    feat: insert createAddress with validation in relations

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -20,5 +20,19 @@ export class CityService {
         },
       }),
     );
+  }
+
+  async findById(cityId: number): Promise<CityEntity> {
+    const city = await this.cityRepository.findOne({
+      where: {
+        id: cityId,
+      },
+    });
+
+    if (!city) {
+      throw new NotFoundException(`CityId: ${cityId}, not found.`);
+    }
+
+    return city;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUserDto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -26,5 +26,19 @@ export class UserService {
 
   async getAllUser(): Promise<UserEntity[]> {
     return this.userRepository.find();
+  }
+
+  async findById(userId: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`UserId: ${userId}, not found`);
+    }
+
+    return user;
   }
 }
